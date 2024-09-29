76fc9d3a123277d5539316ed0990c10367a8de5c
Ali Abdulrahman <ali.abdulrahman@cyshield.com>
2024-09-29 22:19:47 +0300

    update base url

--- app/helper/services/blog.api.ts
+++ app/helper/services/blog.api.ts
@@ -1,3 +1,8 @@
+const baseUrl =
+process.env.NODE_ENV === 'production'
+  ? process.env.NEXT_PUBLIC_BASE_URL // Production URL
+  : 'http://localhost:3000'; // Development URL
+  
 export async function fetchBlogFeaturedArticles(category?: string) {
   const response = await fetch(
     `/api/blogFeaturedArticles${category ? `?category=${category}` : ""}`
@@ -7,19 +12,19 @@ export async function fetchBlogFeaturedArticles(category?: string) {
 }
 
 export async function fetchBlogPopularArticles() {
-  const response = await fetch(`/api/blogPopularArticles`);
+  const response = await fetch(`${baseUrl}/api/blogPopularArticles`);
   const articles = await response.json();
   return articles;
 }
 
 export async function fetchBlogRecentArticles(pageNumber: string = "1") {
-  const response = await fetch(`/api/blogRecentArticles?pn=${pageNumber}`);
+  const response = await fetch(`${baseUrl}/api/blogRecentArticles?pn=${pageNumber}`);
   const articles = await response.json();
   return articles;
 }
 
 export async function fetchBlogArticleDetails(id: string) {
-  const response = await fetch(`/api/blogArticleDetails?id=${id}`);
+  const response = await fetch(`${baseUrl}/api/blogArticleDetails?id=${id}`);
   const article = await response.json();
   return article;
 }
