b1001f194ede7a84847eb41af7a86d630e74e3ce
Ali Abdulrahman <ali.abdulrahman@cyshield.com>
2024-09-29 22:10:00 +0300

    update services call api

--- app/helper/services/blog.api.ts
+++ app/helper/services/blog.api.ts
@@ -7,22 +7,19 @@ export async function fetchBlogFeaturedArticles(category?: string) {
 }
 
 export async function fetchBlogPopularArticles() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-  const response = await fetch(`${baseUrl}/api/blogPopularArticles`);
+  const response = await fetch(`/api/blogPopularArticles`);
   const articles = await response.json();
   return articles;
 }
 
 export async function fetchBlogRecentArticles(pageNumber: string = "1") {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-  const response = await fetch(`${baseUrl}/api/blogRecentArticles?pn=${pageNumber}`);
+  const response = await fetch(`/api/blogRecentArticles?pn=${pageNumber}`);
   const articles = await response.json();
   return articles;
 }
 
 export async function fetchBlogArticleDetails(id: string) {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-  const response = await fetch(`${baseUrl}/api/blogArticleDetails?id=${id}`);
+  const response = await fetch(`/api/blogArticleDetails?id=${id}`);
   const article = await response.json();
   return article;
 }
